afd378e035ed638f42b1a9684eade60895100a23
localadmin <localadmi@server.telmex.com>
2013-01-10 13:02:03 -0600

    Method read_partida in tarifa_model.php corrected

--- php/application/models/tarifa_model.php
+++ php/application/models/tarifa_model.php
@@ -74,11 +74,13 @@ class Tarifa_model extends CI_Model {
 
     public function read_partida($capitulo){
         if ($capitulo){
-            $partidas = $this->db->query('SELECT DISTINCT(partida), partida_desc, partida_a_desc FROM tarifa WHERE capitulo=\'' . $capitulo . '\'');
+            $partidas = $this->db->query('
+                SELECT DISTINCT (partida_desc) FROM (SELECT DISTINCT(partida), partida_desc, partida_a_desc FROM tarifa WHERE capitulo=\'' . $capitulo . '\' ORDER BY partida) as nueva');
             return $partidas->result_array();
         }
         else {
-            $partidas = $this->db->query('SELECT DISTINCT(partida), partida_desc, partida_a_desc FROM tarifa');
+            $partidas = $this->db->query('
+               SELECT DISTINCT (partida_desc) FROM (SELECT DISTINCT(partida), partida_desc, partida_a_desc FROM tarifa ORDER BY partida) as nueva');
             return $partidas->result_array();
         }
     }
